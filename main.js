f1d5b831e72e50c7cc4b3d2f1ef7990dc2c9e82f
Francois56789 <126235118+Franna2@users.noreply.github.com>
2023-05-29 07:10:45 +0200

    done

---
+++ main.js
@@ -0,0 +1,21 @@
+// Smooth scrolling to section on link click
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  for (const link of navLinks) {
+    link.addEventListener("click", smoothScroll);
+  }
+
+  function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth"
+      });
+    }
+  }
+});
